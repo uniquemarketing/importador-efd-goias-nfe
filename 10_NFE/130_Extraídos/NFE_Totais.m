b231ca757c8ada53668ca8a5c99302e5f1370b11
Anderson Andrade <anderson@uniquemarketing.com.br>
2026-03-06 21:40:32 -0300

    refactor(nfe): remover tipagem paralela e ID_Nota de NFE_Totais

--- 10_NFE/130_Extraídos/NFE_Totais.m
+++ 10_NFE/130_Extraídos/NFE_Totais.m
@@ -6,26 +6,6 @@ let
     DerivadosBase = Record.FieldOrDefault(LAYOUT_NFE_BASE, "Derivados", {}),
     TodasColunas = List.Transform(CamposBase, each _[Alias]) & List.Transform(DerivadosBase, each _[Name]),
     Expandir = Table.ExpandTableColumn(AddTabelaNF, "ConteudoProcessado", TodasColunas),
-    SemDuplicadas = Table.Distinct(Expandir, {"Chave de Acesso"}),
-    // Tipagem Dinâmica
-    Regras = List.Transform(
-        CamposBase & List.Buffer(DerivadosBase),
-        each
-            {
-                if _[Alias]? <> null then
-                    _[Alias]
-                else
-                    _[Name],
-                if _[Kind] = "number" then
-                    type number
-                else if _[Kind] = "date" then
-                    type datetime
-                else
-                    type text
-            }
-    ),
-    TabelaTipada = Table.TransformColumnTypes(SemDuplicadas, Regras),
-    // Chave Primária Numérica (SK)
-    ID_Nota = Table.AddIndexColumn(TabelaTipada, "ID_Nota", 1, 1, Int64.Type)
+    SemDuplicadas = Table.Distinct(Expandir, {"Chave de Acesso"})
 in
-    ID_Nota
+    SemDuplicadas
