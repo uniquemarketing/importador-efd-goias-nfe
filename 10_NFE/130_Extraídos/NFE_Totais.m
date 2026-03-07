7bcd2bb669b4b8706d8e7fd6f4c6e435ce1e0bfa
Anderson Andrade <anderson@uniquemarketing.com.br>
2026-03-06 21:42:34 -0300

    refactor(nfe): aplicar contrato ativo na materializacao de totais

--- 10_NFE/130_Extraídos/NFE_Totais.m
+++ 10_NFE/130_Extraídos/NFE_Totais.m
@@ -2,8 +2,9 @@ let
     source = #"102_NFE_Arquivos",
     // Sua consulta que lista os arquivos da pasta
     AddTabelaNF = Table.AddColumn(source, "ConteudoProcessado", each fnNFeProcessarXmlTable([XmlTable])),
-    CamposBase = LAYOUT_NFE_BASE[Fields],
-    DerivadosBase = Record.FieldOrDefault(LAYOUT_NFE_BASE, "Derivados", {}),
+    ContratoBase = NFE_Contrato_Ativo_BASE,
+    CamposBase = ContratoBase[Fields],
+    DerivadosBase = Record.FieldOrDefault(ContratoBase, "Derivados", {}),
     TodasColunas = List.Transform(CamposBase, each _[Alias]) & List.Transform(DerivadosBase, each _[Name]),
     Expandir = Table.ExpandTableColumn(AddTabelaNF, "ConteudoProcessado", TodasColunas),
     SemDuplicadas = Table.Distinct(Expandir, {"Chave de Acesso"})
